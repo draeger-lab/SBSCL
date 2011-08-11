134cc92cb45f6cb654bf377cfde970c1b5f7c5f9
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2011-08-11 14:49:43 +0000

    Added the simulation core algorithm for the SBMLsimulator.

---
+++ src/org/sbml/simulator/math/odes/FastProcessDESystem.java
@@ -0,0 +1,41 @@
+/*
+ * $Id: FastProcessDESystem.java 100 2011-03-11 09:46:15Z draeger $
+ * $URL: https://rarepos.cs.uni-tuebingen.de/svn-path/SBMLsimulator/trunk/src/org/sbml/simulator/math/odes/FastProcessDESystem.java $
+ * ---------------------------------------------------------------------
+ * This file is part of SBMLsimulator, a Java-based simulator for models
+ * of biochemical processes encoded in the modeling language SBML.
+ *
+ * Copyright (C) 2007-2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package org.sbml.simulator.math.odes;
+
+/**
+ * @author Andreas Dr&auml;ger
+ * @author Alexander D&ouml;rr
+ * @date 2010-09-27
+ * @version $Rev: 100 $
+ * @since 1.0
+ */
+public interface FastProcessDESystem extends DESystem {
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean containsFastProcesses();
+
+	/**
+	 * 
+	 * @param isProcessing
+	 */
+	public void setFastProcessComputation(boolean isProcessing);
+	
+}
