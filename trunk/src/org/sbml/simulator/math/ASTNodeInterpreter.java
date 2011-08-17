8ad44c0900efd62fc15b4696972276410c6aa261
andreas-draeger <andreas-draeger@b84df434-5a7c-40cc-91aa-e1166ec6aedb>
2011-08-17 14:32:44 +0000

    Added license header and included logger into SBMLinterpreter.

--- trunk/src/org/sbml/simulator/math/ASTNodeInterpreter.java
+++ trunk/src/org/sbml/simulator/math/ASTNodeInterpreter.java
@@ -1,3 +1,20 @@
+/*
+ * $Id$   
+ * $URL$    
+ * ---------------------------------------------------------------------    
+ * This file is part of SBMLsimulator, a Java-based simulator for models    
+ * of biochemical processes encoded in the modeling language SBML.    
+ *    
+ * Copyright (C) 2007-2011 by the University of Tuebingen, Germany.   
+ *    
+ * This library is free software; you can redistribute it and/or modify   
+ * it under the terms of the GNU Lesser General Public License as   
+ * published by the Free Software Foundation. A copy of the license   
+ * agreement is provided in the file named "LICENSE.txt" included with    
+ * this software distribution and also available online as    
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.    
+ * ---------------------------------------------------------------------
+ */
 package org.sbml.simulator.math;
 
 import java.util.Hashtable;
@@ -22,6 +39,12 @@ import org.sbml.jsbml.util.Maths;
 import org.sbml.jsbml.util.compilers.ASTNodeCompiler;
 import org.sbml.jsbml.util.compilers.ASTNodeValue;
 
+/**   
+ * @author Roland Keller    
+ * @author Andreas Dr&auml;ger    
+ * @version $Rev$   
+ * @since 1.0   
+ */
 public class ASTNodeInterpreter implements ASTNodeCompiler {
   
   /**
