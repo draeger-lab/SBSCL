3675554d72230ad2615313b9ad66bdeee2630514
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2012-04-24 11:28:24 +0000

    Updated all license headers, created a build.xml file and edited the publication.

---
+++ src/org/simulator/About.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of Simulation Core Library, a Java-based library
+ * for efficient numerial simulation of biological models.
+ *
+ * Copyright (C) 2007-2012 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package org.simulator;
+
+import java.awt.HeadlessException;
+
+/**
+ * This class displays a short about message for this library.
+ * 
+ * @author Andreas Dr&auml;ger
+ * @version $Rev$
+ * @since 0.9
+ */
+public class About {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String message = 
+				"This is the Simulation Core Library\n\n" +
+				"Main authors: Andreas Dr\u00e4ger, Roland Keller, and Alexander D\u00f6rr\n" +
+				"Copyright (C) 2007-2012 by the University of Tuebingen,\nT\u00fcbingen, Germany.\n\n" +
+				"License: LGPL Version 3.1\n" +
+				"For more information please see\n" +
+				"http://www.cogsys.cs.uni-tuebingen.de/software/SBMLsimulator/.";
+		try {
+			javax.swing.JOptionPane.showMessageDialog(null, message, "About Simulation Core Library", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+		} catch (HeadlessException exc) {
+			System.out.println(message);
+		}
+	}
+
+}
--- src/org/simulator/math/odes/GraggBulirschStoerSolver.java
+++ src/org/simulator/math/odes/GraggBulirschStoerSolver.java
@@ -2,10 +2,10 @@
  * $Id$
  * $URL$
  * ---------------------------------------------------------------------
- * This file is part of SBMLsimulator, a Java-based simulator for models
- * of biochemical processes encoded in the modeling language SBML.
+ * This file is part of Simulation Core Library, a Java-based library
+ * for efficient numerial simulation of biological models.
  *
- * Copyright (C) 2007-2011 by the University of Tuebingen, Germany.
+ * Copyright (C) 2007-2012 by the University of Tuebingen, Germany.
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
