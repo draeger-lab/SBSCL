30acace255d04fd3f80c3db87302ddad30d28847
otter606 <otter606@b84df434-5a7c-40cc-91aa-e1166ec6aedb>
2012-04-29 22:47:57 +0000

    documents

--- trunk/src/org/simulator/sedml/ExecutionException.java
+++ trunk/src/org/simulator/sedml/ExecutionException.java
@@ -1,5 +1,10 @@
 package org.simulator.sedml;
 
+/**
+ * Runtime exception indicating that SEDML execution has failed.
+ * @author radams
+ *
+ */
 public class ExecutionException extends RuntimeException {
 
 	public ExecutionException(String message) {
