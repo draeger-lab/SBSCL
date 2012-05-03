ee6c8ad581749dc5d972771f216d6aeafbdb802b
andreas-draeger <andreas-draeger@b84df434-5a7c-40cc-91aa-e1166ec6aedb>
2012-05-03 11:19:03 +0000

    Added new license header, set SVN properties where missing, corrected the manuscript for publication.

--- trunk/src/org/simulator/About.java
+++ trunk/src/org/simulator/About.java
@@ -5,7 +5,12 @@
  * This file is part of Simulation Core Library, a Java-based library
  * for efficient numerical simulation of biological models.
  *
- * Copyright (C) 2007-2012 by the University of Tuebingen, Germany.
+ * Copyright (C) 2007-2012 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. The Keio University, Japan
+ * 3. The Harvard University, USA
+ * 4. The University of Edinburgh
+ * 5. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -36,9 +41,9 @@ public class About {
 				"This is the Simulation Core Library\n\n" +
 				"Main authors: Andreas Dr\u00e4ger, Roland Keller, and Alexander D\u00f6rr\n" +
 				"Copyright (C) 2007-2012 by the University of Tuebingen,\nT\u00fcbingen, Germany.\n\n" +
-				"License: LGPL Version 3.1\n" +
+				"License: LGPL Version 3\n" +
 				"For more information please see\n" +
-				"http://www.cogsys.cs.uni-tuebingen.de/software/SBMLsimulator/.";
+				"https://sourceforge.net/projects/simulation-core.";
 		try {
 			javax.swing.JOptionPane.showMessageDialog(null, message, "About Simulation Core Library", javax.swing.JOptionPane.INFORMATION_MESSAGE);
 		} catch (HeadlessException exc) {
