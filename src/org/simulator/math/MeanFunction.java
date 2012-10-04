0c8c040901d25c849aa90e4e3e7d3f2b95087a76
"keller" <roland.keller@gmx.de>
2012-10-04 14:39:55 +0000

    Distance functions added.

---
+++ src/org/simulator/math/MeanFunction.java
@@ -0,0 +1,72 @@
+/*
+ * $$Id${file_name} ${time} ${user} $$
+ * $$URL${file_name} $$
+ * ---------------------------------------------------------------------
+ * This file is part of Simulation Core Library, a Java-based library
+ * for efficient numerical simulation of biological models.
+ *
+ * Copyright (C) 2007-2012 jointly by the following organizations:
+ * 1. University of Tuebingen, Germany
+ * 2. Keio University, Japan
+ * 3. Harvard University, USA
+ * 4. The University of Edinburgh, UK
+ * 5. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package org.simulator.math;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Roland Keller
+ * @version $Rev$
+ * @since 1.0
+ * @date 2011-03-25
+ */
+public abstract class MeanFunction implements Serializable {
+	
+	/**
+	 * Generated serial identifier.
+	 */
+	private static final long serialVersionUID = -7272984374334773096L;
+	
+	/**
+	 * @param values
+	 * @return
+	 */
+	public abstract double computeMean(double... values);
+	
+	/**
+	 * @param values
+	 * @return
+	 */
+	public double computeMean(List<Double> values) {
+		double[] val = new double[values.size()];
+		for (int i = 0; i != val.length; i++) {
+			val[i] = values.get(i);
+		}
+		return computeMean(val);
+	}
+	
+	/**
+	 * @param x
+	 * @return
+	 */
+	public double computeMean(Iterable<? extends Number> values) {
+		List<Double> val = new ArrayList<Double>();
+		for (Number number : values) {
+			val.add(number.doubleValue());
+		}
+		return computeMean(val);
+	}
+
+}
---
+++ src/org/simulator/math/Relative_N_Metric.java
@@ -0,0 +1,140 @@
+/*
+ * $$Id${file_name} ${time} ${user} $$
+ * $$URL${file_name} $$
+ * ---------------------------------------------------------------------
+ * This file is part of Simulation Core Library, a Java-based library
+ * for efficient numerical simulation of biological models.
+ *
+ * Copyright (C) 2007-2012 jointly by the following organizations:
+ * 1. University of Tuebingen, Germany
+ * 2. Keio University, Japan
+ * 3. Harvard University, USA
+ * 4. The University of Edinburgh, UK
+ * 5. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package org.simulator.math;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import de.zbit.util.ResourceManager;
+
+
+/**
+ * @author Roland Keller
+ * @version $Rev$
+ * @since 1.0
+ */
+public class Relative_N_Metric extends QualityMeasure {
+	
+	/**
+	 * Generated serial version identifier.
+	 */
+	private static final long serialVersionUID = 5066304615795368201L;
+	
+	/**
+	 * 
+	 */
+	private static final ResourceBundle bundle = ResourceManager.getBundle("org.sbml.simulator.locales.Simulator");
+	
+	/**
+	 * Is default value NaN? (for faster computation)
+	 */
+	private boolean defaultNaN;
+	
+	/**
+	 * 
+	 */
+	protected N_Metric metric;
+
+	/**
+	 * 
+	 */
+	public Relative_N_Metric() {
+		super(Double.NaN);
+		metric = new N_Metric();
+		defaultNaN = true;
+	}
+	
+	/**
+	 * 
+	 * @param root
+	 */
+	public Relative_N_Metric(double root) {
+		super(Double.NaN);
+		metric = new N_Metric(root);
+		defaultNaN = true;
+	}
+
+	/**
+	 * 
+	 * @param metric
+	 */
+	public Relative_N_Metric(N_Metric metric) {
+		super(Double.NaN);
+		this.metric = metric;
+		if(Double.isNaN(metric.getDefaultValue())) {
+			defaultNaN = true;
+		}
+		else {
+			defaultNaN = false;
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.sbml.simulator.math.Distance#distance(java.lang.Iterable, java.lang.Iterable, double)
+	 */
+	public double distance(Iterable<? extends Number> x,
+			Iterable<? extends Number> expected, double defaultValue) {
+		double numerator=metric.distance(x, expected, defaultValue);
+		List<Double> nullVector = new LinkedList<Double>();
+		for(@SuppressWarnings("unused") Number n : expected) {
+			nullVector.add(0d);
+		}
+		double denominator=metric.distance(expected,nullVector,defaultValue);
+		double denominator2=metric.distance(x,nullVector,defaultValue);
+		if ((denominator != 0) && (denominator2 != 0) ) {
+			return numerator / denominator;
+		} else if((denominator == 0) && (denominator2 == 0)){
+			return numerator;
+		} else if(defaultNaN) {
+				return numerator;
+		}
+		else {
+			return this.defaultValue;
+		}
+	}
+
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.sbml.simulator.math.QualityMeasure#setDefaultValue(double)
+	 */
+	public void setDefaultValue(double value) {
+		super.setDefaultValue(value);
+		if(Double.isNaN(defaultValue)) {
+			defaultNaN = true;
+		}
+		else {
+			defaultNaN = false;
+		}
+	}
+
+	/**
+	 * @param double1
+	 */
+	public void setRoot(double root) {
+		metric.setRoot(root);
+	}
+
+
+}
