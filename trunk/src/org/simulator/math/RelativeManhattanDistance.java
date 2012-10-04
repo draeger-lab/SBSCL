5158c42223060e73421216d8f36217c66fd5b527
"keller" <"keller"@b84df434-5a7c-40cc-91aa-e1166ec6aedb>
2012-10-04 15:09:25 +0000

    CSVImporter and simulation test added. SysBio is now needed.

--- trunk/src/org/simulator/math/RelativeManhattanDistance.java
+++ trunk/src/org/simulator/math/RelativeManhattanDistance.java
@@ -1,6 +1,6 @@
 /*
- * $$Id${file_name} ${time} ${user} $$
- * $$URL${file_name} $$
+ * $Id: RelativeManhattanDistance.java 246 2012-10-04 14:39:55Z "keller"$
+ * $URL: svn+ssh://svn.code.sf.net/p/simulation-core/code/trunk/src/org/simulator/math/RelativeManhattanDistance.java$
  * ---------------------------------------------------------------------
  * This file is part of Simulation Core Library, a Java-based library
  * for efficient numerical simulation of biological models.
