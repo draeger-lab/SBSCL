f8c36e3d88985bf1c70254a1eba344c3ab88a7bc
andreas-draeger <andreas-draeger@b84df434-5a7c-40cc-91aa-e1166ec6aedb>
2013-02-15 09:28:39 +0000

    A copy of the new web site is now part of the repository and all the JavaDocs have been moved to the web directory.

--- trunk/src/org/simulator/sbml/astnode/AssignmentRuleValue.java
+++ trunk/src/org/simulator/sbml/astnode/AssignmentRuleValue.java
@@ -22,107 +22,105 @@
  */
 package org.simulator.sbml.astnode;
 
-
-
 import java.util.Map;
 import org.sbml.jsbml.Species;
 import org.simulator.sbml.SBMLValueHolder;
 
 /**
- * This class can compute and store the value of an AssignmentRule together with the variable of the rule.
+ * This class can compute and store the value of an AssignmentRule together with
+ * the variable of the rule.
+ * 
  * @author Roland Keller
  * @version $Rev$
  */
-public class AssignmentRuleValue extends RuleValue{
-  /**
-   * The id of the species reference the rule is referring to (if applicable)
-   */
+public class AssignmentRuleValue extends RuleValue {
+
+	/**
+	 * The id of the species reference the rule is referring to (if applicable)
+	 */
 	private String speciesReferenceID;
-	
+
 	/**
-	 * Array for saving the old Y value
+	 * The map of the values of the species references that are contained in
+	 * rules
 	 */
-	private double[] oldY;
-	
+	private Map<String,Double> stoichiometricCoefHash;
+
 	/**
-	 * The map of the values of the species references that are contained in rules
+	 * 
+	 * @param nodeObject
+	 * @param index
 	 */
-  private Map<String,Double> stoichiometricCoefHash;
+	public AssignmentRuleValue(ASTNodeValue nodeObject, int index) {
+		super(nodeObject, index);
+	}
 
-  
-  /**
-   * 
-   * @param nodeObject
-   * @param index
-   */
-  public AssignmentRuleValue(ASTNodeValue nodeObject, int index) {
-    super(nodeObject, index);
-  }
+	/**
+	 * Constructor for rules that refer to a species reference
+	 * @param nodeObject
+	 * @param speciesReferenceID
+	 * @param stoichiometricCoefHash
+	 */
+	public AssignmentRuleValue(ASTNodeValue nodeObject, String speciesReferenceID,
+			Map<String, Double> stoichiometricCoefHash) {
+		super(nodeObject,-1);
+		this.speciesReferenceID = speciesReferenceID;
+		this.stoichiometricCoefHash = stoichiometricCoefHash;
+	}
 
-  /**
-   * Constructor for rules that refer to a species reference
-   * @param nodeObject
-   * @param speciesReferenceID
-   * @param stoichiometricCoefHash
-   */
-  public AssignmentRuleValue(ASTNodeValue nodeObject, String speciesReferenceID,
-    Map<String, Double> stoichiometricCoefHash) {
-    super(nodeObject,-1);
-    this.speciesReferenceID = speciesReferenceID;
-    this.stoichiometricCoefHash = stoichiometricCoefHash;
-  }
-  
-  /**
-   * Constructor for rules that refer to a species
-   * @param nodeObject
-   * @param index
-   * @param sp
-   * @param compartmentIndex
-   * @param hasZeroSpatialDimensions
-   * @param valueHolder
-   */
-  public AssignmentRuleValue(ASTNodeValue nodeObject, int index,
-    Species sp, int compartmentIndex, boolean hasZeroSpatialDimensions, SBMLValueHolder valueHolder) {
-    super(nodeObject, index, sp, compartmentIndex, hasZeroSpatialDimensions, valueHolder);
-  }
+	/**
+	 * Constructor for rules that refer to a species
+	 * @param nodeObject
+	 * @param index
+	 * @param sp
+	 * @param compartmentIndex
+	 * @param hasZeroSpatialDimensions
+	 * @param valueHolder
+	 */
+	public AssignmentRuleValue(ASTNodeValue nodeObject, int index,
+			Species sp, int compartmentIndex, boolean hasZeroSpatialDimensions, SBMLValueHolder valueHolder) {
+		super(nodeObject, index, sp, compartmentIndex, hasZeroSpatialDimensions, valueHolder);
+	}
 
-  /**
-   * Processes the rule and saves the new value of the corresponding variable in the Y vector if changeY is set to true.
-   * @param Y
-   * @param time
-   * @param changeY
-   * @return Has there been a change in the Y vector caused by the rule?
-   */
-  public boolean processRule(double[] Y, double time, boolean changeY) {
-    if(index>=0) {
-      double oldValue=Y[index];
-      processAssignmentVariable(time);
-      if(changeY) {
-        Y[index] = value;
-      }
-      if(oldValue!=value) {
-        return true;
-      }
-    }
-    else if(speciesReferenceID!=null) {
-      processAssignmentVariable(time);
-      Double v=stoichiometricCoefHash.get(speciesReferenceID);
-      stoichiometricCoefHash.put(speciesReferenceID, value);
-      
-      if((v!=null) && (v.doubleValue()!=value)) {
-        return true;
-      }
-    }
-    return false;
-    
-  }
-  
-  /**
-   * Returns the id of the species reference (if present), null otherwise.
-   * @return id
-   */
-  public String getSpeciesReferenceID() {
-    return speciesReferenceID;
-  }
-  
+	/**
+	 * Processes the rule and saves the new value of the corresponding variable
+	 * in the Y vector if changeY is set to true.
+	 * 
+	 * @param Y
+	 * @param time
+	 * @param changeY
+	 * @return Has there been a change in the Y vector caused by the rule?
+	 */
+	public boolean processRule(double[] Y, double time, boolean changeY) {
+		if(index >= 0) {
+			double oldValue = Y[index];
+			processAssignmentVariable(time);
+			if (changeY) {
+				Y[index] = value;
+			}
+			if (oldValue != value) {
+				return true;
+			}
+		}
+		else if (speciesReferenceID != null) {
+			processAssignmentVariable(time);
+			Double v=stoichiometricCoefHash.get(speciesReferenceID);
+			stoichiometricCoefHash.put(speciesReferenceID, value);
+
+			if ((v != null) && (v.doubleValue() != value)) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	/**
+	 * Returns the id of the species reference (if present), null otherwise.
+	 * @return id
+	 */
+	public String getSpeciesReferenceID() {
+		return speciesReferenceID;
+	}
+
 }
