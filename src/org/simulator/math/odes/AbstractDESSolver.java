58cdf5f4c407cf9af68d55f290d698e549ef2f9e
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2013-09-07 00:42:57 +0000

    Changed the extraction of the svn revision number in the build script because this does not work with some versions of svn. Commented the use of a logger, which in some cases could produce null access.

--- src/org/simulator/math/odes/AbstractDESSolver.java
+++ src/org/simulator/math/odes/AbstractDESSolver.java
@@ -1020,9 +1020,9 @@ public abstract class AbstractDESSolver implements DelayValueHolder, DESSolver, 
 			System.arraycopy(yTemp, 0, result[i], 0, yTemp.length);
 
 			v = additionalResults(DES, t, yTemp, data, i);
-			if (logger.getLevel().intValue() < Level.INFO.intValue()) {
-				logger.fine("additional results: " + Arrays.toString(v));
-			}
+//			if ((logger != null) && (logger.getLevel().intValue() < Level.INFO.intValue())) {
+//				logger.fine("additional results: " + Arrays.toString(v));
+//			}
 			firePropertyChange(timePoints[i-1] * intervalFactor, timePoints[i] * intervalFactor);
 			
 			t = timePoints[i];
