1c4b3f170b05c2b8e4e92c2945e92307e0db6959
Shalin Shah <sns37@duke.edu>
2018-05-16 18:12:33 -0400

    Initial implementation of plot library

---
+++ src/org/simulator/plot/plotMultiTable.java
@@ -0,0 +1,60 @@
+package org.simulator.plot;
+
+import org.jfree.chart.ChartPanel;
+import java.util.Iterator;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.ui.ApplicationFrame;
+import org.simulator.math.odes.MultiTable;
+import org.simulator.math.odes.MultiTable.Block.Column;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+/**
+ * This class adds output data-plot support to SBSCL
+ * 
+ * @author Shalin Shah
+ * @since 1.5
+ */
+@SuppressWarnings("serial")
+public class plotMultiTable extends ApplicationFrame {
+	private MultiTable species;
+	private DefaultCategoryDataset data;
+	
+	/**
+	   * Initializes the JFreeChart and dataSet for the chart using MultiTable
+	   * 
+	   * @param MultiTable
+	   *        The input data type to the plot API is MultiTable which gets converted
+	   *        internally to DefaultCategoryDataset
+	   */
+	public plotMultiTable(MultiTable table) {
+		super("Model simulation");
+		
+		species = table;
+		JFreeChart lineChart = ChartFactory.createLineChart("chart title", 
+				"time", "conentration (nM)", createDataset(),
+				PlotOrientation.VERTICAL, true, true, false);
+
+		ChartPanel chartPanel = new ChartPanel( lineChart );
+		chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
+		setContentPane(chartPanel);
+		
+	}
+
+   private DefaultCategoryDataset createDataset() {
+	    data = new DefaultCategoryDataset();
+	   
+	   
+	   for(int i = 1; i < species.getColumnCount(); i++) {
+		   Column col = species.getColumn(i);
+		    
+		   int time_step = 0;
+		   for (Iterator<Double> iter = col.iterator(); iter.hasNext(); time_step++){
+			      data.addValue(iter.next().doubleValue(), col.getColumnName(),  String.valueOf(species.getTimePoint(time_step)));
+		   }
+	   }
+		
+      return data;
+   }
+}
