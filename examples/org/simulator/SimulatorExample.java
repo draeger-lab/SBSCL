88e825327d1c6eecd584fb60b8b91f738fcecab7
Shalin Shah <shalinshah1993@gmail.com>
2018-05-29 10:19:41 -0400

    Updated plotMultiTable name

--- examples/org/simulator/SimulatorExample.java
+++ examples/org/simulator/SimulatorExample.java
@@ -1,6 +1,4 @@
 /*
- * $Id$
- * $URL$
  * ---------------------------------------------------------------------
  * This file is part of Simulation Core Library, a Java-based library
  * for efficient numerical simulation of biological models.
@@ -25,7 +23,6 @@
 package org.simulator;
 
 import java.awt.Dimension;
-import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
@@ -36,6 +33,7 @@ import javax.xml.stream.XMLStreamException;
 import org.apache.commons.math.ode.DerivativeException;
 import org.jfree.ui.RefineryUtilities;
 import org.sbml.jsbml.Model;
+import org.sbml.jsbml.SBMLDocument;
 import org.sbml.jsbml.SBMLException;
 import org.sbml.jsbml.validator.ModelOverdeterminedException;
 import org.sbml.jsbml.SBMLReader;
@@ -43,10 +41,8 @@ import org.simulator.math.odes.AbstractDESSolver;
 import org.simulator.math.odes.DESSolver;
 import org.simulator.math.odes.MultiTable;
 import org.simulator.math.odes.RosenbrockSolver;
-import org.simulator.plot.plotMultiTable;
+import org.simulator.plot.PlotMultiTable;
 import org.simulator.sbml.SBMLinterpreter;
-
-import org.simulator.plot.plotMultiTable;
 
 /**
  * A simple program that performs a simulation of a model.
@@ -78,7 +74,9 @@ public class SimulatorExample {
     double timeEnd = Double.parseDouble(args[2]);
 
     // Read the model and initialize solver
-    Model model = SBMLReader.read(new File(fileName)).getModel();
+    SBMLDocument document = (new SBMLReader()).readSBML(fileName);
+    Model model = document.getModel();
+    
     DESSolver solver = new RosenbrockSolver();
     solver.setStepSize(stepSize);
     SBMLinterpreter interpreter = new SBMLinterpreter(model);
@@ -98,7 +96,7 @@ public class SimulatorExample {
         + model.getId(), JOptionPane.INFORMATION_MESSAGE);
     
     // plot all the reactions species
-    plotMultiTable p = new plotMultiTable(solution);
+    PlotMultiTable p = new PlotMultiTable(solution, "Output plot");
     p.pack();
     RefineryUtilities.centerFrameOnScreen(p);
     p.setVisible( true );
