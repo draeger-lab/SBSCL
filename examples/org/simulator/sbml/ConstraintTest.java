f733ea6fd62977fed47bc193fab0115372074ae4
Shalin Shah <shalinshah1993@gmail.com>
2018-05-30 10:24:09 -0400

    Bug fix for Issue#16 on shalinshah1993/SBSCL

--- examples/org/simulator/sbml/ConstraintTest.java
+++ examples/org/simulator/sbml/ConstraintTest.java
@@ -1,6 +1,4 @@
 /*
- * $Id$
- * $URL$
  * ---------------------------------------------------------------------
  * This file is part of Simulation Core Library, a Java-based library
  * for efficient numerical simulation of biological models.
@@ -13,6 +11,7 @@
  * 5. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
  * 6. The University of California, San Diego, La Jolla, CA, USA
  * 7. The Babraham Institute, Cambridge, UK
+ * 8. Duke University, Durham, NC, US
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -21,7 +20,7 @@
  * this software distribution and also available online as
  * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
  * ---------------------------------------------------------------------
- */
+ */ 
 package org.simulator.sbml;
 
 import java.io.IOException;
@@ -45,6 +44,7 @@ import org.simulator.math.odes.RosenbrockSolver;
  * contains a {@link Constraint}.
  * 
  * @author Andreas Dr&auml;ger
+ * @author Shalin Shah
  * @version $Rev$
  * @since 1.3
  */
@@ -61,11 +61,12 @@ public class ConstraintTest {
    * @throws DerivativeException
    */
   public static void main(String args[]) throws XMLStreamException, IOException, ParseException, SBMLException, ModelOverdeterminedException, DerivativeException {
-    SBMLDocument doc = SBMLReader.read(ConstraintTest.class.getResourceAsStream("data/Constraint-00001-L3V1.xml"));
+	  // Configuration
+    String fileName = args[0];
+    double stepSize = Double.parseDouble(args[1]);
+    double timeEnd = Double.parseDouble(args[2]);
 
-    double stepSize = .1d;
-    double timeEnd = 5d;
-
+    SBMLDocument doc = SBMLReader.read(fileName);
     DESSolver solver = new RosenbrockSolver();
     solver.setStepSize(stepSize);
     SBMLinterpreter interpreter = new SBMLinterpreter(doc.getModel());
