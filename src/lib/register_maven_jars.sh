7423d1e1c20224d544c4137469e7e3122ba88d35
Matthias König <konigmatt@googlemail.com>
2018-06-07 19:28:18 +0200

    Fix #19, external jars added as artifacts

---
+++ src/lib/register_maven_jars.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#################################################
+# Script for registering maven artifacts
+#
+# If additional jars have to be added register
+# these in the same manner like below in the
+# repository.
+#################################################
+
+# lib directory
+LIB_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# register
+mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -DgroupId=scpsolver -DartifactId=SCPSolver -Dversion=1.0 -Dfile=nmi/scpsolver/1.0/SCPSolver-1.0.jar -DlocalRepositoryPath=${LIB_DIR}/maven -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
+mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -DgroupId=scpsolver -DartifactId=GLPKSolverPack -Dversion=4.35 -Dfile=nmi/scpsolver/1.0/GLPKSolverPack-4.35.jar -DlocalRepositoryPath=${LIB_DIR}/maven -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
+mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -DgroupId=scpsolver -DartifactId=LPSOLVESolverPack -Dversion=5.5.2.5 -Dfile=nmi/scpsolver/1.0/LPSOLVESolverPack-5.5.2.5.jar -DlocalRepositoryPath=${LIB_DIR}/maven -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
