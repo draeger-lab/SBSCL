b8d753b973acdd85b288bc7028adecee4a055606
Matthias Koenig <konigmatt@googlemail.com>
2018-06-13 13:18:53 +0200

    script downloading sbml-test-suite

---
+++ src/test/download_sbml-test-suite.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#################################################
+# Download sbml-test-suite for testing
+#
+# usage:
+#  source ./download_sbml-test-suite.sh
+#################################################
+
+TEST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# download and extract the bigg models for testing
+mkdir -p resources/sbml-test-suite
+cd resources/sbml-test-suite
+wget https://github.com/sbmlteam/sbml-test-suite/releases/download/3.3.0/sbml-semantic-test-cases-2017-12-12.zip
+unzip sbml-semantic-test-cases-2017-12-12.zip
+rm sbml-semantic-test-cases-2017-12-12.zip
+
+# set environment variable
+export SBML_TEST_CASES=${TEST_DIR}/resources/sbml-test-suite/cases/semantic
+
+echo $SBML_TEST_CASES
+
+# move back
+cd $TEST_DIR
+
+
+
+
+
+
