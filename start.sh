7e6bcfe325a1a9908f29fbcaa1aba182869a1689
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2020-04-08 22:55:48 +0530

    updated the start.sh file

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp libsbmlj.jar:colt.jar:concurrent.jar:jdom.jar:fern.jar fern.Start $@
+java -cp lib/libsbmlj.jar:lib/colt/colt-2.1.4.jar:lib/colt/concurrent-1.3.4.jar:lib/jdom-1.1.3.jar:lib/fern.jar fern.Start $@
