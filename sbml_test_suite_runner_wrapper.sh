ba828ead61f2aacb58221b3a02ffa6b8aa33ffea
Matthias Koenig <konigmatt@googlemail.com>
2020-05-28 16:38:31 +0200

    bugfix for relative paths

--- sbml_test_suite_runner_wrapper.sh
+++ sbml_test_suite_runner_wrapper.sh
@@ -6,4 +6,7 @@
 #  source ./sbml_test_suite_runner_wrapper.sh
 #################################################
 
-java -cp target/classes/:src/lib/sbml_test_runner_wrapper/* org.simulator.examples.SBMLTestSuiteRunnerWrapper $1 $2 $3 $4 $5
+# lib directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+java -cp $DIR/target/classes/:$DIR/src/lib/sbml_test_runner_wrapper/* org.simulator.examples.SBMLTestSuiteRunnerWrapper $1 $2 $3 $4 $5
