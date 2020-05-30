bc20db6610af6e9675a0bfbd8ca01638711769cf
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2020-05-30 22:16:01 +0530

    add scripts folder under src/test folder

--- src/test/scripts/download_bigg_models.sh
+++ src/test/scripts/download_bigg_models.sh
@@ -6,19 +6,23 @@
 #  source ./download_bigg_models.sh
 #################################################
 
+# This URL gives the updated BiGG Models from the repository
+BIGG_MODELS_BASE_URL="https://github.com/matthiaskoenig/bigg-models-fba/raw/master/models"
+FILE_NAME="bigg_models_v1.5.tar.gz"
+
 _CWD="$PWD"
 TEST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # download and extract the bigg models for testing
-cd TEST_DIR
-mkdir -p $TEST_DIR/resources/bigg
-cd $TEST_DIR/resources/bigg
-wget https://github.com/matthiaskoenig/bigg-models-fba/raw/master/models/bigg_models_v1.5.tar.gz
+cd $TEST_DIR/../resources/bigg
+mkdir -p $TEST_DIR/../resources/bigg
+cd $TEST_DIR/../resources/bigg
+wget $BIGG_MODELS_BASE_URL/$FILE_NAME
 tar xzf bigg_models_v1.5.tar.gz
 rm bigg_models_v1.5.tar.gz
 
 # set environment variable
-export BIGG_MODELS_PATH=${TEST_DIR}/resources/bigg/v1.5
+export BIGG_MODELS_PATH=${TEST_DIR}/../resources/bigg/v1.5
 
 echo $BIGG_MODELS_PATH
 
--- src/test/scripts/download_sbml-test-suite.sh
+++ src/test/scripts/download_sbml-test-suite.sh
@@ -6,26 +6,25 @@
 #  source ./download_sbml-test-suite.sh
 #################################################
 
+# This URL gives the updated SBML test cases from SBML Test Suite repository
 SBML_TEST_SUITE_LINK="https://github.com/sbmlteam/sbml-test-suite/branches/develop/cases"
 
 _CWD="$PWD"
 TEST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # download and extract the bigg models for testing
-cd $TEST_DIR
-rm -rf $TEST_DIR/resources/sbml-test-suite
-mkdir -p $TEST_DIR/resources/sbml-test-suite
-cd $TEST_DIR/resources/sbml-test-suite
+cd $TEST_DIR/../resources/sbml-test-suite
+rm -rf $TEST_DIR/../resources/sbml-test-suite
+mkdir -p $TEST_DIR/../resources/sbml-test-suite
+cd $TEST_DIR/../resources/sbml-test-suite
 svn checkout $SBML_TEST_SUITE_LINK
 cd cases
 rm NEWS.md
 
 # set environment variable
-export SBML_TEST_SUITE_PATH=${TEST_DIR}/resources/sbml-test-suite/cases/semantic/
+export SBML_TEST_SUITE_PATH=${TEST_DIR}/../resources/sbml-test-suite/cases/semantic/
 
 echo $SBML_TEST_SUITE_PATH
 
 # move back
 cd $_CWD
-
-exit 0
