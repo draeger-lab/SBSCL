beb2b429977bbc28de9fbc4fd7830bc48e8b9e42
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2021-02-23 00:21:35 +0530

    update BiGG models script to download models from updated BiGG database

--- src/test/scripts/download_bigg_models.sh
+++ src/test/scripts/download_bigg_models.sh
@@ -9,8 +9,8 @@
 set -eo pipefail
 
 # This URL gives the updated BiGG Models from the repository
-BIGG_MODELS_BASE_URL="https://github.com/matthiaskoenig/bigg-models-fba/raw/master/models"
-FILE_NAME="bigg_models_v1.5.tar.gz"
+BIGG_MODELS_BASE_URL="https://www.dropbox.com/sh/ye05djxrpxy37da/AAAPwL27xQ97mHGoLmCxhI-4a/v1.6"
+FILE_NAME="models.tgz"
 
 _CWD="$PWD"
 TEST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -18,13 +18,12 @@ TEST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # download and extract the bigg models for testing
 cd $TEST_DIR/../resources/bigg
 wget $BIGG_MODELS_BASE_URL/$FILE_NAME
-tar xzf $FILE_NAME
+tar -xvzf $FILE_NAME --wildcards '*.xml'
 rm $FILE_NAME
-cd $TEST_DIR/../resources/bigg/v1.5/
-gunzip *.gz
+cd $TEST_DIR/../resources/bigg/models/
 
 # set environment variable
-export BIGG_MODELS_PATH=${TEST_DIR}/../resources/bigg/v1.5
+export BIGG_MODELS_PATH=${TEST_DIR}/../resources/bigg/models
 
 echo $BIGG_MODELS_PATH
 
